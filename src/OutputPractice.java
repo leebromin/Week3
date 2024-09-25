1c7e4c50287c487fa5504243363314b0511c1ba3
이형민 <mini100142@gmail.com>
2024-09-25 16:24:06 +0900

    20240925

---
+++ src/OutputPractice.java
@@ -0,0 +1,13 @@
+public class OutputPractice {
+    public static void main(String[] args) {
+        int age = 22;
+        String hobby = "노는게 제일 좋아";
+        String name = "이형민";
+
+        System.out.println(name = "(" + age + ")" + hobby);
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age + "세");
+        System.out.println("Hobby: " + hobby);
+    }
+}
